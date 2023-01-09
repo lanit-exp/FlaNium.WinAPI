32b5c9fc36e590d6eb55f4a3d7b2caa4511ace94
Uhanov Maxim <cmttwd@gmail.com>
2023-01-09 23:02:08 +0500

    FlaNiumDriverService fix for selenium 4

--- src/main/java/FlaNium/WinAPI/webdriver/FlaNiumDriverService.java
+++ src/main/java/FlaNium/WinAPI/webdriver/FlaNiumDriverService.java
@@ -1,4 +1,5 @@
 package FlaNium.WinAPI.webdriver;
+
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import org.openqa.selenium.Capabilities;
@@ -7,6 +8,9 @@ import org.openqa.selenium.remote.service.DriverService;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -20,6 +24,7 @@ public class FlaNiumDriverService extends DriverService {
 
     /**
      * Creates a default instance of the FlaNiumDriverService using a default path to the FlaNium Desktop Driver.
+     *
      * @return A {@link FlaNiumDriverService} using FlaNium Desktop and random port
      */
     public static FlaNiumDriverService createDesktopService() {
@@ -27,9 +32,9 @@ public class FlaNiumDriverService extends DriverService {
     }
 
 
-    protected FlaNiumDriverService(File executable, int port, ImmutableList<String> args,
-                                   ImmutableMap<String, String> environment) throws IOException {
-        super(executable, port, args, environment);
+    protected FlaNiumDriverService(File executable, int port, Duration timeout, List<String> args,
+                                   Map<String, String> environment) throws IOException {
+        super(executable, port, timeout, args, environment);
     }
 
     public static class Builder extends DriverService.Builder<FlaNiumDriverService, Builder> {
@@ -45,6 +50,7 @@ public class FlaNiumDriverService extends DriverService {
         private boolean verbose = false;
         private boolean silent = false;
         private final int DEFAULT_PORT = 9999;
+        private Duration timeout = Duration.ofSeconds(20);
 
         @Override
         public int score(Capabilities capabilities) {
@@ -88,6 +94,17 @@ public class FlaNiumDriverService extends DriverService {
         }
 
         /**
+         * Set timeout waiting for driver server to start.
+         *
+         * @param timeout Timeout waiting for driver server to start.
+         * @return A self reference.
+         */
+        public Builder withTimeout(Duration timeout) {
+            this.timeout = timeout;
+            return this;
+        }
+
+        /**
          * Creates a new {@link FlaNiumDriverService} to manage the FlaNium Desktop Driver server.
          * Before creating a new service, the builder will find a port for the server to listen to.
          *
@@ -104,12 +121,11 @@ public class FlaNiumDriverService extends DriverService {
             }
 
             try {
-                return new FlaNiumDriverService(exe, port, createArgs(), ImmutableMap.<String, String>of());
+                return new FlaNiumDriverService(exe, port, timeout, createArgs(), ImmutableMap.of());
             } catch (IOException e) {
                 throw new WebDriverException(e);
             }
         }
-
 
 
         @Override
@@ -118,7 +134,7 @@ public class FlaNiumDriverService extends DriverService {
         }
 
         @Override
-        protected ImmutableList<String> createArgs() {
+        protected List<String> createArgs() {
             if (getLogFile() == null) {
                 String logFilePath = System.getProperty(FLANIUM_DRIVER_LOG_PATH_PROPERTY);
                 if (logFilePath != null) {
@@ -144,10 +160,10 @@ public class FlaNiumDriverService extends DriverService {
         }
 
         @Override
-        protected FlaNiumDriverService createDriverService(File exe, int port, ImmutableList<String> args,
-                                                           ImmutableMap<String, String> environment) {
+        protected FlaNiumDriverService createDriverService(File exe, int port, Duration timeout, List<String> args,
+                                                           Map<String, String> environment) {
             try {
-                return new FlaNiumDriverService(exe, port, args, environment);
+                return new FlaNiumDriverService(exe, port, timeout, args, environment);
             } catch (IOException e) {
                 throw new WebDriverException(e);
             }
